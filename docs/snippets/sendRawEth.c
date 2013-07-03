5fc850c44d64eeb0c05c5b9d92a209204bc2bb5c
rtubio <rtubio@marajota.com.uvigo.es>
2013-07-03 18:09:06 +0200

    * sendRawEth.c :: improved RAW transmission.

--- docs/snippets/sendRawEth.c
+++ docs/snippets/sendRawEth.c
@@ -17,15 +17,8 @@
 #include <net/if.h>
 #include <netinet/ether.h>
 
-#define MY_DEST_MAC0	0x00
-#define MY_DEST_MAC1	0x11
-#define MY_DEST_MAC2	0x22
-#define MY_DEST_MAC3	0x33
-#define MY_DEST_MAC4	0x44
-#define MY_DEST_MAC5	0x55
-
 #define DEFAULT_IF	"eth0"
-#define BUF_SIZ		1024
+#define BUF_SIZ		2048
 
 #define USE_WRITE 1
 #define GN_ETHTYPE 0x0707
@@ -44,66 +37,109 @@ const unsigned char ETH_ADDR_FAKE[ETH_ALEN]
 const unsigned char ETH_ADDR_S1A[ETH_ALEN]
                                     = { 0x00, 0x04, 0x5F, 0x03, 0xED, 0xCA };
 
+int print_hex_data(const char *buffer, const int len);
+
+typedef struct llc_header
+{
+	uint16_t ssap;
+	uint16_t dsap;
+}
+llc_header_t;
+
+#define LEN__ETH_HEADER 	sizeof(struct ethhdr)
+#define LEN__LLC_HEADER_T 	sizeof(llc_header_t)
+
+#define LEN__HEADERS 		( LEN__ETH_HEADER + LEN__LLC_HEADER_T )
+#define LEN__DATA 			1024
+#define LEN__FRAME			LEN__HEADERS + LEN__DATA
+
 int main(int argc, char *argv[])
 {
 
 	int sockfd;
+	uint16_t lsap = htons(ETH_P_IP);
 	struct ifreq if_idx;
 	struct ifreq if_mac;
-	int tx_len = 0;
+
 	char sendbuf[BUF_SIZ];
-	struct ether_header *eh = (struct ether_header *) sendbuf;
-	struct iphdr *iph = (struct iphdr *) (sendbuf + sizeof(struct ether_header));
+	struct ethhdr *eh = (struct ethhdr *) sendbuf;
+	llc_header_t *llc_header = (llc_header_t *) ( sendbuf + LEN__ETH_HEADER );
+	char *data = (char *) sendbuf + LEN__HEADERS;
+
 	char ifName[IFNAMSIZ];
 
 	/* Get interface name */
-	if (argc > 1)
-		strcpy(ifName, argv[1]);
-	else
-		strcpy(ifName, DEFAULT_IF);
+	if (argc > 1) strcpy(ifName, argv[1]);
+	else strcpy(ifName, DEFAULT_IF);
 
 	/* Open RAW socket to send on */
-	if ((sockfd = socket(AF_PACKET, SOCK_RAW, IPPROTO_RAW)) == -1) {
-	    perror("socket");
+	//IPPROTO_RAW changed by htons(GN_ETHTYPE)
+	if ( (sockfd = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL))) < 0 )
+	{
+	    perror("Could not create socket...");
+	    exit(-1);
 	}
 
 	/* Get the index of the interface to send on */
 	memset(&if_idx, 0, sizeof(struct ifreq));
 	strncpy(if_idx.ifr_name, ifName, IFNAMSIZ-1);
 	if (ioctl(sockfd, SIOCGIFINDEX, &if_idx) < 0)
-	    perror("SIOCGIFINDEX");
+	{
+	    perror("SIOCGIFINDEX, Could not retrieve IF index...");
+	    exit(-1);
+	}
 	/* Get the MAC address of the interface to send on */
 	memset(&if_mac, 0, sizeof(struct ifreq));
 	strncpy(if_mac.ifr_name, ifName, IFNAMSIZ-1);
 	if (ioctl(sockfd, SIOCGIFHWADDR, &if_mac) < 0)
-	    perror("SIOCGIFHWADDR");
+	{
+	    perror("SIOCGIFHWADDR, Could not retrieve MAC address...");
+	    exit(-1);
+	}
 
 	/* Construct the Ethernet header */
 	memset(sendbuf, 0, BUF_SIZ);
 	/* Ethernet header */
-	memcpy(eh->ether_shost, &(if_mac.ifr_hwaddr.sa_data), ETH_ALEN);
-	memcpy(eh->ether_dhost, ETH_ADDR_S1A, ETH_ALEN);
+	memcpy(eh->h_source, &(if_mac.ifr_hwaddr.sa_data), ETH_ALEN);
+	memcpy(eh->h_dest, ETH_ADDR_S1A, ETH_ALEN);
 	/* Ethertype field */
-	eh->ether_type = htons(GN_ETHTYPE);
+	eh->h_proto = lsap;
 
-	tx_len += sizeof(struct ether_header);
+	/* LLC header */
+	printf("*** dsap = %d, eth_h = %p, llc_h = %p, d = %d\n"
+				, htons(GN_ETHTYPE), eh, llc_header
+				, (int)((void *)llc_header - (void *)eh));
+	printf("*** LEN__ETH_HEADER = %d, LEN__HEADERS = %d\n"
+				, LEN__ETH_HEADER, LEN__HEADERS);
+	llc_header->dsap = lsap;
+	llc_header->ssap = lsap;
 
 	/* Packet data */
-	sendbuf[tx_len++] = 0xde;
-	sendbuf[tx_len++] = 0xad;
-	sendbuf[tx_len++] = 0xbe;
-	sendbuf[tx_len++] = 0xef;
+	int i = 0;
+	for ( i = 0; i < LEN__DATA; i++ )
+		{ data[i] = i; }
+
+	printf(">>>>> Packet created:\n");
+	print_hex_data(sendbuf, LEN__FRAME);
+	printf("\n");
 
 	/* Send packet */
-	int b_written = 0;
 	struct sockaddr_ll socket_address;
-	socket_address.sll_family = PF_PACKET;
-	socket_address.sll_ifindex = if_idx.ifr_ifindex;
-	//socket_address.sll_protocol = htons(ETH_P_LOOP);
+
+	socket_address.sll_family 	= PF_PACKET;
+	socket_address.sll_ifindex 	= if_idx.ifr_ifindex;
+	socket_address.sll_protocol	= lsap;
+	/*ARP hardware identifier is ethernet*/
+	socket_address.sll_hatype   = ARPHRD_ETHER;
+	/*target is another host*/
+	socket_address.sll_pkttype  = PACKET_OTHERHOST;
 
 #ifndef USE_WRITE
 	socket_address.sll_halen = ETH_ALEN;
+	memcpy((void*)(socket_address.sll_addr), (void*)ETH_ADDR_S1A, ETH_ALEN);
 #endif
+
+	int b_written = 0;
 
 #ifdef USE_WRITE
 
@@ -115,12 +151,12 @@ int main(int argc, char *argv[])
 	    exit(-1);
 	}
 
-	if ( ( b_written = write(sockfd, sendbuf, tx_len) ) < 0 )
+	if ( ( b_written = write(sockfd, sendbuf, LEN__FRAME) ) < 0 )
 
 #else
 
 	if ( ( b_written = sendto(	sockfd,
-								sendbuf, tx_len, 0,
+								sendbuf, LEN__FRAME, 0,
 								(struct sockaddr*)&socket_address,
 								sizeof(struct sockaddr_ll))	) < 0	)
 
@@ -134,4 +170,29 @@ int main(int argc, char *argv[])
 	printf("Packet sent!, Bytes written = %d\n", b_written);
 
 	return 0;
+}
+
+#define BYTES_PER_LINE 14	/*!< Number of bytes per line to be printed. */
+
+/* print_eth_data */
+int print_hex_data(const char *buffer, const int len)
+{
+
+	int last_byte = len - 1;
+
+	if ( len < 0 )
+		{ return(-1); }
+
+	int i = 0;
+
+	for ( i = 0; i < len; i++ )
+	{
+		if ( ( i % BYTES_PER_LINE ) == 0 )
+			{ printf("\n\t\t\t"); }
+		printf("%02X", 0xFF & (unsigned int)buffer[i]);
+		if ( i < last_byte ) { printf(":"); }
+	}
+
+	return(0);
+
 }
